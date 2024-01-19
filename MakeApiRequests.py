ce502bfbc45e868d35425f861082708c5220c49f
SheenKocher <156446657+SheenKocher@users.noreply.github.com>
2024-01-19 15:30:57 +0530

    Add files via upload

---
+++ MakeApiRequests.py
@@ -0,0 +1,51 @@
+import requests
+import json
+class Api:
+    def __init__(self):
+        pass
+
+    def makeApiRequestForCounrty(self, country_name):
+        url = "https://covid-193.p.rapidapi.com/statistics"
+        querystring = {"country": country_name}
+        headers = {
+            'x-rapidapi-host': "covid-193.p.rapidapi.com",
+            'x-rapidapi-key': "117215c277msh0a42ca2a900f570p1e84b2jsn18169c535974"
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        result = js.get('response')[0]
+        print(result.get('cases'))
+        print("*" * 20)
+        return result.get('cases') , result.get('deaths'),result.get('tests')
+
+
+    def makeApiRequestForIndianStates(self):
+        url = "https://covid19-data.p.rapidapi.com/india"
+        headers = {
+            'x-rapidapi-host': "covid19-data.p.rapidapi.com",
+            'x-rapidapi-key': "117215c277msh0a42ca2a900f570p1e84b2jsn18169c535974"
+        }
+        response = requests.request("GET", url, headers=headers)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        #result = js.get('list')
+        return js
+
+
+    def makeApiWorldwide(self):
+        url = "https://covid-19-statistics.p.rapidapi.com/reports/total"
+        headers = {
+            "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+            "x-rapidapi-key": "117215c277msh0a42ca2a900f570p1e84b2jsn18169c535974"
+        }
+        response = requests.request("GET", url, headers=headers)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        result = js.get('data')
+
+        return result
+
